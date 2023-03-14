12e218ec343e359fc264aabb1c4ab3a1e4e5f27a
nguyen.ong <nguyen.ong@oivan.com>
2023-03-14 18:10:50 +0700

    update order

--- hooks/Order/useCreateOrder.ts
+++ hooks/Order/useCreateOrder.ts
@@ -2,18 +2,25 @@ import { useLoading } from '../../context/loading/index';
 import { createOrderApi } from "@/api/orders";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from '@/utils/toast';
+import useUserCart from '../Cart/useUserCart';
+import { useUIContext } from '@/context/ui';
 import useOrders from './useOrders';
 
 const useAddOrder = () => {
-  const { refetch } = useOrders()
+  const { refetch } = useUserCart()
+  const { refetch: refetchOrder } = useOrders()
   const { setLoading } = useLoading()
+  const { setShowCart } = useUIContext()
 
   return useMutation(createOrderApi, {
     onMutate: () => {
       setLoading(true)
     },
     onSuccess: () => {
+      toast("Created Order successfully", `success`)
       refetch()
+      refetchOrder()
+      setShowCart(false)
     },
     onError: () => {
       toast("Failed to add Order", `error`)
